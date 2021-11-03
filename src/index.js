7b6cc78a88f741520d7acbe8bbe5d81f93614660
Hamza <hamzatarar8@gmail.com>
2021-11-03 08:33:46 +0200

    Completed html structure and styling with css. Added to do list tasks dynamically.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,38 @@
-import _ from 'lodash';
 import './style.css';
+const tasks = document.querySelector('.tasks');
 
-function component() {
-  const element = document.createElement('div');
+const tasksArray = [
+  {
+    description: 'Thinking of a fat joint',
+    completed: false,
+    index: 0,
+  },
+  {
+    description: 'Rolling a fat joint',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Smoking of a fat joint',
+    completed: false,
+    index: 2,
+  },
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  return element;
-}
-
-document.body.appendChild(component());
+function render() {
+  for (let i = 0; i < tasksArray.length; i +=1) {
+    tasks.innerHTML += `
+    <li class="displayList">
+      <div class="innerItems">
+        <input type="checkbox">
+        <p>${tasksArray[i].description}</p>
+      </div>
+      <div class="innerItems">
+        <button class="listButton"><i class='fas fa-ellipsis-v'></i></button>
+        <button class="binButton"><i class="material-icons" id="bin">delete</i></button>
+      </div>
+    </li>
+    `;
+  }
+};
+window.addEventListener('load', render());
